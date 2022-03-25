82e2efd974bb589d0971678cf289c7e9b8694068
Дукенбай Урзия Әбдіхалыққызы <Urziya.Dukenbay@kaspi.kz>
2022-03-25 18:05:05 +0600

    в первом коммите разобрались с данными, этот обработка событии

--- HelloWorld/main.js
+++ HelloWorld/main.js
@@ -2,5 +2,10 @@ new Vue({
     el: '#app',
     data: {
         title: 'Hello World! First Vue...'
+    },
+    methods: {
+        handleChange(e) {
+            this.title = e.target.value;
+        }
     }
 });
