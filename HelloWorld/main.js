667a4e402a7c2266d9dac3b5310fb8dc3145b122
Дукенбай Урзия Әбдіхалыққызы <Urziya.Dukenbay@kaspi.kz>
2022-04-06 15:11:16 +0600

    change onclick

--- HelloWorld/main.js
+++ HelloWorld/main.js
@@ -3,11 +3,19 @@ new Vue({
     data: {
         title: 'Hello World! First Vue...',
         href: "https://www.google.com/",
-        link: "<a href='loftschool.com'>ссылка</a>"
+        link: "<a href='loftschool.com'>ссылка</a>",
+        example: {
+            title: 'Hello World!!!',
+            coordX: 0
+        }
     },
     methods: {
         handleChange(e) {
             this.title = e.target.value;
+        },
+        onChange(newTitle, e) {
+            this.example.title = newTitle;
+            this.example.coordX = e.clientX;
         }
     }
 });
