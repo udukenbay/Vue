4f552b55747ba90dfc68f77362edfb8675ffee2f
Дукенбай Урзия Әбдіхалыққызы <Urziya.Dukenbay@kaspi.kz>
2022-04-06 16:23:09 +0600

    get and set in computer property

--- Interpolation/main.js
+++ Interpolation/main.js
@@ -5,12 +5,26 @@ new Vue({
     },
     computed: {
         reversedTitle() {
-            return this.title.split('').reverse().join('');
+            return this.title.split('').reverse().join('')
+        },
+        newTitle: {
+            get() {
+                return 'new title';
+            },
+            set(value) {
+                console.log('computed prop seted for', value);
+                this.title = 'setted value';
+            }
         }
     },
     methods: {
         reverseTitle(title) {
-            return title.split('').reverse().join('');
+            return title.split('').reverse().join('')
         },
-    },
+        setupTitle() {
+            // this.title = this.newTitle
+            this.newTitle = 'vaaaaaluuuue'
+            this.title = this.newTitle
+        }
+    }
 });
