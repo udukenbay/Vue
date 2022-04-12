354ca16bfc8d97782b28afd9e550b95a04c66e87
Дукенбай Урзия Әбдіхалыққызы <Urziya.Dukenbay@kaspi.kz>
2022-04-12 11:51:51 +0600

    inline стили

---
+++ InlineStyles/main.js
@@ -0,0 +1,13 @@
+new Vue({
+    el: '#app',
+    data: {
+        title: 'Hello World!!!',
+        styles: {
+            color: 'white',
+            backgroundColor: 'blue',
+            fontSize: '20px'
+        },
+        fontNewColor: 'blue',
+        backgroundNewColor: 'pink'
+    },
+});
