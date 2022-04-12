a7b2b70456b37278e8b5610ca029b0b148a130f0
Дукенбай Урзия Әбдіхалыққызы <Urziya.Dukenbay@kaspi.kz>
2022-04-12 12:53:59 +0600

    list render

---
+++ RenderList/main.js
@@ -0,0 +1,15 @@
+new Vue({
+    el: '#app',
+    data: {
+        arr: ['one', 'two', 'three'],
+        obj: {
+            prop1: 'val1',
+            prop2: 'val2'
+        },
+        arrObj: [
+            {name: 'John', salary : 2000},
+            {name: 'Foo', salary : 500},
+            {name: 'Bar', salary : 1000},
+        ]
+    },
+});
