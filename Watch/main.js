91e8bf21518c6d1bf115bcb5ead1d19ee56958f8
Дукенбай Урзия Әбдіхалыққызы <Urziya.Dukenbay@kaspi.kz>
2022-04-12 11:43:04 +0600

    classes

--- Watch/main.js
+++ Watch/main.js
@@ -2,17 +2,20 @@ new Vue({
     el: '#app',
     data: {
         title: 'Hello World!!!',
+        activeClass: 'active',
+        errorClass: 'error',
         changed: false
     },
     methods: {
         setupTitle() {
-            this.title = "new title"
+            this.changed = !this.changed
+            // this.title = "new title"
         }
     },
-    watch: {
-        title(value) {
-            // ввиде методов написать те свойства за которыми хотим следить - у нас это title
-            this.changed = true
-        }        
-    }
+    // watch: {
+    //     title(value) {
+    //         // ввиде методов написать те свойства за которыми хотим следить - у нас это title
+    //         this.changed = true
+    //     }        
+    // }
 });
